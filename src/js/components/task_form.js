f3cb2457838b577b7e7f013ece67103065f53ec4
Chris Keathley <c@keathley.io>
2014-10-24 22:32:31 -0400

    Add initial flux stucture
    This adds all of the basic pieces needed to get things working with the
    flux architecture.

--- src/js/components/task_form.js
+++ src/js/components/task_form.js
@@ -3,11 +3,31 @@
  */
 
 var React = require('react');
+var TaskActions = require('../actions/task_actions');
 
 var TaskForm = React.createClass({
-
+  getInitialState: function() {
+    return {
+      text: ''
+    };
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    TaskActions.create(this.state.text);
+    this.setState({text: ''});
+  },
+  onChange: function(event) {
+    this.setState({
+      text: event.target.value
+    });
+  },
   render: function() {
     return (
+      <form className="task-form" onSubmit={this.handleSubmit}>
+        <input onChange={this.onChange}
+          value={this.state.text}
+          placeholder="What needs doing..." />
+      </form>
     );
   }
 
